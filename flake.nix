b448775b7b7ff34ce59ddc0ff74bcbceb7ae2892
hugosenari <hugosenari@gmail.com>
2022-03-12 00:51:09 -0300

    chore: re index blimp

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = ''Utility that helps with big files in git, very similar to git-fat, s3annnex etc.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
